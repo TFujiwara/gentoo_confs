c3ba1b611c0eb8467e03fa727fe8ed799796aa17
TFujiwara <cristianometal94@gmail.com>
2021-11-15 18:41:34 +0100

    Update. Cambio a Stage1.sh

--- instalar.sh
+++ instalar.sh
@@ -41,28 +41,6 @@ mount --rbind /dev $dir/dev
 mkdir $dir/usr/portage
 # chroot al nuevo sistema (de forma temporal)
 chroot $dir /bin/bash
-source /etc/profile
-export PS1="(chroot) $PS1"
-# Actualizar portage
-emerge-webrsync
-emerge --sync --quiet
-# Elegir perfil de instalación, descomentar esta linea una vez se tenga claro.
-# eselect profile set 
-# Actualizar @world
-emerge -uaDN @world
-# Setear zona horaria
-echo "Europe/Madrid" > /etc/timezone
-emerge --config sys-libs/timezone-data
-# Local config (Lenguaje)
-# Descargar y generar Kernel
-# Editar fstab
-# Configurar red
-# root password
-# Establecer teclado
-# Configurar reloj del sistema
-# Programas necesarios
-# Instalacion de GRUB 
-# Salir de chroot, desmontar particiones y reiniciar
 else
 if [ -d "$dir2"]
 then
