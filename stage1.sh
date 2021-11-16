921f287cc3897290e917fdc31f912ed8889e7c84
TFujiwara <cristianometal94@gmail.com>
2021-11-16 09:37:35 +0100

    Update stage1.sh

--- stage1.sh
+++ stage1.sh
@@ -8,7 +8,7 @@ cores=$(cat /proc/cpuinfo | grep "cpu cores" | cut -c 13)
 if [ -d "$dir" ]
 then 
 echo "El directorio donde está montado el disco del sistema existe, continuando..."
-cd /mnt/gentoo
+cd /mnt/sys
 # Elegid de aqui el paquete de stage 3 que os interese, esto lo actualizare conforme cambien las versiones
 # Stage 3 desktop sin hardenizar
 # wget https://mirror.bytemark.co.uk/gentoo//releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-desktop-openrc-20211114T170549Z.tar.xz
@@ -48,10 +48,43 @@ else
 if [ -d "$dir2"]
 then
 echo "El directorio donde está montado el disco del sistema existe, continuando..."
+echo "El directorio donde está montado el disco del sistema existe, continuando..."
 cd /mnt/gentoo
-wget https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20211114T170549Z/stage3-amd64-openrc-20211114T170549Z.tar.xz
+# Elegid de aqui el paquete de stage 3 que os interese, esto lo actualizare conforme cambien las versiones
+# Stage 3 desktop sin hardenizar
+# wget https://mirror.bytemark.co.uk/gentoo//releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-desktop-openrc-20211114T170549Z.tar.xz
+# Stage 3 hardenizado
+# wget https://mirror.bytemark.co.uk/gentoo//releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-hardened-openrc-20211114T170549Z.tar.xz
+# Stage 3 hardenizado nomultilib
+# wget https://mirror.bytemark.co.uk/gentoo//releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-hardened-nomultilib-openrc-20211114T170549Z.tar.xz
+# Stage 3 hardenizado con SELinux
+# https://mirror.bytemark.co.uk/gentoo//releases/amd64/autobuilds/current-stage3-amd64-desktop-openrc/stage3-amd64-hardened-selinux-openrc-20211114T170549Z.tar.xz
 tar -xvf stage3-*.tar.xz
+# Recordar al usuario la necesidad de configurar la flag USE si necesita algo más especifico que un sistema base sin X
+echo "Te recuerdo que tienes que configurar la flag USE si necesitas algo más que un sistema base sin X (entorno gráfico), que es lo que se instala aqui. O puedes configurarlo posteriormente!"
 echo "Cores a configurar en /etc/portage/make.conf: " $cores
+# Parte de descarga de configuraciones/tweaks de gentoo desde el repo
+
+# wget del make.conf en la carpeta /etc/portage del punto de montaje
+# Descomentar una vez decidido que procesador estamos usando
+# wget -O $dir2/etc/portage/make.conf https://raw.githubusercontent.com/TFujiwara/gentoo_confs/main/<procesador>.conf
+
+# Disparamos el editor nano en pantalla para que el usuario configure los cores y la flag USE en caso de necesitarlo
+nano $dir2/etc/portage/make.conf
+# Copiamos el resolv.conf de la imagen LiveCD al sistema, advertimos que esto se puede modificar para poner los DNS que queramos
+cp -L /etc/resolv.conf $dir2/etc/resolv.conf
+echo "Se ha copiado el resolv.conf del LiveCD que estes usando al sistema. Puedes modificarlo si crees conveniente para poner otros DNS."
+# Montar los sistemas de archivos necesarios para compilar
+mount -t proc none $dir2/proc
+mount --rbind /sys $dir2/sys
+mount --rbind /dev $dir2/dev
+# Creamos el directorio portage en /usr/
+mkdir $dir2/usr/portage
+# Instalamos genfstab en el LiveCD de Gentoo y generamos fstab
+emerge sys-fs/genfstab
+genfstab -U -p $dir2 >> $dir2/etc/fstab
+# chroot al nuevo sistema (de forma temporal)
+chroot $dir2 /bin/bash
 else
 echo "No se ha encontrado ningún directorio candidato a la instalación..."
 exit 0 
